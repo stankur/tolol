a85b2cde83e7ebe14f235a204763c37154f1e02f
Stanley Kurniawan <terserahskurniawan@gmail.com>
2024-07-14 13:01:30 +0700

    added ticks and made detail level smaller

--- src/lib/components/DetailLevel/DetailLevel.svelte
+++ src/lib/components/DetailLevel/DetailLevel.svelte
@@ -3,8 +3,11 @@
 	export let level: number;
 </script>
 
-<div class="flex flex-col align-top gap-2 p-4 rounded-md bg-neutral-100  sticky top-2" on:click>
+<div class="flex flex-col align-top gap-3 p-2 w-1/3 rounded-md bg-neutral-100  sticky top-2" on:click>
+    <div>
     <Range bind:value={level} min={0} max={100} step={50} on:click={(e) => e.stopPropagation()}/>
-        <div></div>
+    
+    <div class="flex justify-between text-xs pl-2 pr-2"><span>1</span> <span>2</span> <span>3</span></div>
+    </div>
 	<div class="text-xs"> Detail Level </div>
 </div>
