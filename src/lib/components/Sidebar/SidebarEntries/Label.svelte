a972418bb131261517475ecaba63668b74c4e192
Stanley Kurniawan <terserahskurniawan@gmail.com>
2024-07-14 09:07:18 +0700

    base path stuffs

--- src/lib/components/Sidebar/SidebarEntries/Label.svelte
+++ src/lib/components/Sidebar/SidebarEntries/Label.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import { base } from '$app/paths';
     export let label: string;
     export let url: string;
+
 </script>
 
-<a href={url} class="flex grow justify-left items-center pl-4 pr-4 hover:bg-neutral-200 rounded-lg overflow-hidden">
+<a href={base + url} class="flex grow justify-left items-center pl-4 pr-4 hover:bg-neutral-200 rounded-lg overflow-hidden">
     <span class="inline-block whitespace-nowrap overflow-ellipsis overflow-hidden" >{label}</span>
 </a>
--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/site" : "",
+			base: process.env.NODE_ENV === "production" ? "/tolol" : "",
 		},
         alias: {
             $compiler: path.resolve('./src/compiler'),
